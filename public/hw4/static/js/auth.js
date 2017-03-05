976c16ad75d92e0fca8ac3e50ce94b74a1d39ec3
dylanuys <duys@ucsd.edu>
2017-03-04 18:37:28 -0800

    figuring out how to use firebase

--- public/hw4/static/js/auth.js
+++ public/hw4/static/js/auth.js
@@ -2,38 +2,43 @@ const auth = firebase.auth();
 
 
 const btnSignup = document.getElementById('btnSignup');
-const signupUsername = document.getElementById('signupUsername');
-const signupEmail = document.getElementById('signupEmail');
-const signupPass = document.getElementById('singupPassword');
-const signupPass2 = document.getElementById('singupPasswordRetype');
 
-btnSignup.addEventListener('click', e => {
-    const email = signupEmail.value;
-    const username = singupUsername.value;
-    const pass = signupPass.value;
-    const pass2 = signupPass2.value;
+if (btnSignup != null) {
+    const signupUsername = document.getElementById('signupUsername');
+    const signupEmail = document.getElementById('signupEmail');
+    const signupPass = document.getElementById('singupPassword');
+    const signupPass2 = document.getElementById('singupPasswordRetype');
 
-    // create user and log them in
-    if (pass == pass2) {
-	const promise = auth.createUserWithEmailAndPassword(email, pass);
-	promise.catch(e => console.log(e.message));
-    }
-
-});
+    btnSignup.addEventListener('click', e => {
+	const email = signupEmail.value;
+	const username = singupUsername.value;
+	const pass = signupPass.value;
+	const pass2 = signupPass2.value;
+	
+	// create user and log them in
+	if (pass == pass2) {
+	    const promise = auth.createUserWithEmailAndPassword(email, pass);
+	    promise.catch(e => console.log(e.message));
+	}
+    });
+}
 
 const btnLogin = document.getElementById('btnLogin');
-const loginUsername = document.getElementById('loginUsername');
-const loginEmail = document.getElementById('loginEmail');
-const loginPass = document.getElementById('loginPassword');
 
-btnSignup.addEventListener('click', e => {
-    const email = loginEmail.value;
-    const pass = loginPass.value;
+if (btnLogin != null) {
+    const loginUsername = document.getElementById('loginUsername');
+    const loginEmail = document.getElementById('loginEmail');
+    const loginPass = document.getElementById('loginPassword');
 
-    // log in an existing user
-    const promise = auth.createUserWithEmailAndPassword(email, pass);
-    promise.catch(e => console.log(e.message));
-});
+    btnSignup.addEventListener('click', e => {
+	const email = loginEmail.value;
+	const pass = loginPass.value;
+	
+	// log in an existing user
+	const promise = auth.createUserWithEmailAndPassword(email, pass);
+	promise.catch(e => console.log(e.message));
+    });
+}
 
 // fires when user logs in or out
 auth.onAuthStateChanged(firebaseUser => {
