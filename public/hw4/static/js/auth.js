5019054859bda2c7d763bcf1b85e270ff3984e30
dylanuys <duys@ucsd.edu>
2017-03-04 19:36:22 -0800

    Finished registration

--- public/hw4/static/js/auth.js
+++ public/hw4/static/js/auth.js
@@ -1,24 +1,22 @@
-const auth = firebase.auth();
-
-
 const btnSignup = document.getElementById('btnSignup');
 
 if (btnSignup != null) {
     const signupUsername = document.getElementById('signupUsername');
     const signupEmail = document.getElementById('signupEmail');
-    const signupPass = document.getElementById('singupPassword');
-    const signupPass2 = document.getElementById('singupPasswordRetype');
+    const signupPass = document.getElementById('signupPassword');
+    const signupPass2 = document.getElementById('signupPasswordRetype');
 
-    btnSignup.addEventListener('click', e => {
+     btnSignup.addEventListener('click', e => {
 	const email = signupEmail.value;
-	const username = singupUsername.value;
+	const username = signupUsername.value;
 	const pass = signupPass.value;
 	const pass2 = signupPass2.value;
 	
 	// create user and log them in
-	if (pass == pass2) {
-	    const promise = auth.createUserWithEmailAndPassword(email, pass);
-	    promise.catch(e => console.log(e.message));
+	 if (pass == pass2) {
+	     const auth = firebase.auth();
+	     const promise = auth.createUserWithEmailAndPassword(email, pass);
+	     promise.catch(e => console.log(e.code, e.message));
 	}
     });
 }
@@ -35,13 +33,14 @@ if (btnLogin != null) {
 	const pass = loginPass.value;
 	
 	// log in an existing user
+	const auth = firebase.auth();
 	const promise = auth.createUserWithEmailAndPassword(email, pass);
 	promise.catch(e => console.log(e.message));
     });
 }
 
 // fires when user logs in or out
-auth.onAuthStateChanged(firebaseUser => {
+firebase.auth().onAuthStateChanged(firebaseUser => {
     if (firebaseUser)
 	console.log(firebaseUser);
     else
