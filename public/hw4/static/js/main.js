34edd071916b32ffd35bb66d518e7e2b33e5c23a
Chang <cgwoun21@gmail.com>
2017-03-05 15:08:50 -0800

    working on http requests

--- public/hw4/static/js/main.js
+++ public/hw4/static/js/main.js
@@ -32,5 +32,16 @@ function loadPage(){
 		console.log('Error: ' + error.code);
 	});
 }
-
+/*
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
+*/
 loadPage();
