50685b8b9a65148e572df73508aef53ab5aaded4
Mauricio Panelo <mpanelo@ucsd.edu>
2017-03-04 16:32:26 -0800

    added app.js

---
+++ public/hw4/static/js/app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAPnQcQGlvVwpBLkKk49gjnw6EPuur7Pls",
+    authDomain: "foodex-d5655.firebaseapp.com",
+    databaseURL: "https://foodex-d5655.firebaseio.com",
+    storageBucket: "foodex-d5655.appspot.com",
+    messagingSenderId: "774577145729"
+};
+
+var app = firebase.initializeApp(config);
+var db = app.database()
