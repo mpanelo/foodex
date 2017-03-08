345706565e1cb60e20b596ca729304aa0f6e02fa
Mauricio Panelo <mpanelo@ucsd.edu>
2017-03-07 16:04:56 -0800

    able to change recipe title

--- public/hw4/static/js/editRecipe.js
+++ public/hw4/static/js/editRecipe.js
@@ -1,1 +1,27 @@
-var hello;
+Vue.use(VueFire);
+var editRef = db.ref("recipes/" + readCookie("recipeToEdit"));
+
+window.addEventListener("load", function () {
+	var vm = new Vue({
+		el: "#editApp",
+    data: {
+      difficulty: "",
+      visibility: "",
+      fileName: ""
+    },
+		firebase: {
+			recipe: {
+				source: editRef,
+				asObject: true,
+				cancelCallback: function () {}
+			}
+		},
+    methods: {
+      updateRecipe: function (event) {
+        var prop = event.target.name;
+        var newText = event.target.value;
+        editRef.child(prop).set(newText);
+      }
+    }
+	});
+});
