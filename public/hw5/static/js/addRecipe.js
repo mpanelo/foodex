be85afd56a75a8b733783dba3630b8ced2bf7bd5
Mauricio Panelo <mpanelo@ucsd.edu>
2017-03-15 13:46:37 -0700

    main now displays compressed images

--- public/hw5/static/js/addRecipe.js
+++ public/hw5/static/js/addRecipe.js
@@ -24,7 +24,7 @@ function getDefaultData() {
 function init() {
     var selectedFile = "";
     var resized = "";
-    var smallURL = "";
+    var smallUrl = "";
     var vm = new Vue({
         el: "#addApp",
 
@@ -83,12 +83,31 @@ function init() {
                   ctx.drawImage(img,0,0,width,height);
 
                   canvas.toBlob(function(blob) {
-                    imageRef.child("small_" + selectedFile.name).put(blob);
-                  });
-                  imageRef.child("small_" + selectedFile.name).getDownloadURL().then(function(url) {
-                    console.log("url = " + url);
-
-                  }).catch(function(error) {
+                    var upTask = imageRef.child("small_" + selectedFile.name).put(blob);
+                    // Register three observers:
+                    // 1. 'state_changed' observer, called any time the state changes
+                    // 2. Error observer, called on failure
+                    // 3. Completion observer, called on successful completion
+                    upTask.on('state_changed', function(snapshot) {
+                      // Observe state change events such as progress, pause, and resume
+                      // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                      console.log('Upload is ' + progress + '% done');
+                      switch (snapshot.state) {
+                        case firebase.storage.TaskState.PAUSED: // or 'paused'
+                          console.log('Upload is paused');
+                          break;
+                        case firebase.storage.TaskState.RUNNING: // or 'running'
+                          console.log('Upload is running');
+                          break;
+                      }
+                    }, function(error) {
+                      // Handle unsuccessful uploads
+                    }, function() {
+                      // Handle successful uploads on complete
+                      // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                      smallUrl = upTask.snapshot.downloadURL;
+                    });
                   });
                 };
               };
@@ -144,7 +163,9 @@ function init() {
                    "hours": that.hours,
                    "minutes": that.minutes,
                    "imageName": that.fileName,
+                   "smallImageName": "small" + that.fileName,
                    "imageUrl": downloadURL,
+                   "smallUrl": smallUrl,
                    "uid": that.user.uid
                  });
 
